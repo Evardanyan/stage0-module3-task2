ad36f53fc91c7f7b7826c8d1d557fd92d58ae713
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-23 22:00:06 +0400

    revert

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,6 +1,7 @@
 package lang.print.gaps.task2;
 
-public class Contact {
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
