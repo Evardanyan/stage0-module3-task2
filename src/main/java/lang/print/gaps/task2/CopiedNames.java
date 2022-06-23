cd64a11eae7436ba4ff7da7ffc4e85e5832164a3
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-23 21:44:56 +0400

    done

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int startVal = 0;
+        int tmpVal = 2000;
+        int width = 1000;
+        int profit = 4000;
+        int cost = 3000;
+        int service = 5000;
+        int apartment = 7000;
+        int total = 90000;
     }
 }
