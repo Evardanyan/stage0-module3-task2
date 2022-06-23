024535f50d8b5f20552d172fe0aac8d73ba4ff0e
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-23 21:55:39 +0400

    test solution

--- src/main/java/lang/print/gaps/task2/Contact.java
+++ src/main/java/lang/print/gaps/task2/Contact.java
@@ -1,7 +1,6 @@
 package lang.print.gaps.task2;
 
-public class AdvancedNamingConvention {
-    private static final int ADULT_AGE = 18;
+public class Contact {
 
     private int age;
     private int phoneNumber;
